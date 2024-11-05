c1c60f1ab84fe23102b80d09052bd093a2ed08f3
wangjin <1262853780@qq.com>
2024-11-05 14:16:57 +0800

    提交

---
+++ src/main/java/com/huaHuabiz/HuaHuaBiz.java
@@ -0,0 +1,11 @@
+package com.huaHuabiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HuaHuaBiz {
+    public static void main(String[] args) {
+        SpringApplication.run(HuaHuaBiz.class, args);
+    }
+}
